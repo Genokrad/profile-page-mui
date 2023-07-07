cbcc623aef02439764ac1ba65d4c29c9d2be77e2
Genokrad <zeeper90@gmail.com>
2023-07-07 19:44:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/profilPage">
+    <BrowserRouter basename="/myReactProject">
       <App />
     </BrowserRouter>
   </React.StrictMode>
