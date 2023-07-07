e1ddd44ff9c0e26f2eb8e2675b6ca873c340aee2
Genokrad <zeeper90@gmail.com>
2023-07-07 20:16:25 +0300

    url fix

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/myReactProject">
+    <BrowserRouter basename="/profile-page-mui">
       <App />
     </BrowserRouter>
   </React.StrictMode>
